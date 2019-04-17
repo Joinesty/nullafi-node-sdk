98115866a94679cd2d7b1cc2b13db25e3fc3d426
Jose Souza <jcarlosjunior@live.com>
2019-04-17 11:25:27 -0300

    adding very simple example

--- examples/index.js
+++ examples/index.js
@@ -5,18 +5,17 @@ const NullafiNodeSDK = require('@joinesty/nullafi-node-sdk');
 
 const start = async () => {
     const app = express();
-    const sdk = new NullafiNodeSDK('YOU API KEY');
+    const sdk = new NullafiNodeSDK('WFPaEwyAZRwCjb+eCRbqoGf/Zgo/OcWUx9q6//yfCk0=');
     const client = await sdk.createClient();
 
     const exampleStore = {
         vaultObj: null,
         ssnToken: null,
-        nameToke: null,
+        firstNameToken: null,
         addressToken: null,
     };
 
     app.engine('hbs', handlebars({
-        defaultLayout: 'main',
         extname: '.hbs',
     }));
     app.set('view engine', 'hbs');
@@ -30,15 +29,37 @@ const start = async () => {
         res.render('home', exampleStore);
     });
 
+    app.get('/reset', (req, res) => {
+        exampleStore.vaultObj = null;
+        exampleStore.ssnToken = null;
+        exampleStore.firstNameToken = null;
+        exampleStore.addressToken = null;
+        res.redirect('/');
+    });
+
     app.post('/static/vault', async (req, res) => {
-        exampleStore.vaultObj = await client.addStaticVault(req.body.vaultName, ['example-tag']);
-        res.render('home');
+        exampleStore.vaultObj = await client.addStaticVault(req.body.vaultName, ['example-static-tag']);
+        res.redirect('/');
     });
 
     app.post('/static/ssn', async (req, res) => {
-        exampleStore.vaultObj.
-        res.render('home');
+        exampleStore.ssnToken = await exampleStore.vaultObj.ssn.postSSN(req.body.ssn, ['example-ssn-tag']);
+        res.redirect('/');
     });
+
+    app.post('/static/firstName', async (req, res) => {
+        exampleStore.firstNameToken = await exampleStore.vaultObj.firstName.postFirstName(req.body.firstName, ['example-firstName-tag']);
+        res.redirect('/');
+    });
+
+    app.post('/static/addressToken', async (req, res) => {
+        exampleStore.addressToken = await exampleStore.vaultObj.address.postAddress(req.body.address, ['example-address-tag']);
+        res.redirect('/');
+    });
+
+    app.listen(3000);
+    console.log('Server started!');
+    console.log('Visit http://localhost:3000/ to start.');
 };
 
 start();
