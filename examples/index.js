d5ab8343864871bcd44bd2906286d37d9275aa91
Jose Souza <jcarlosjunior@live.com>
2019-06-05 13:19:54 -0300

    updating SDK

--- examples/index.js
+++ examples/index.js
@@ -10,19 +10,19 @@ const start = async () => {
 
     const exampleStore = {
         vaultObj: null,
-        ssnToken: null,
-        firstNameToken: null,
-        addressToken: null,
-        dateOfBirthToken: null,
-        driversLicenseToken: null,
-        genderToken: null,
-        genericToken: null,
-        lastNameToken: null,
-        passportToken: null,
-        placeOfBirthToken: null,
-        raceToken: null,
-        randomToken: null,
-        vehicleRegistrationToken: null,
+        ssnAlias: null,
+        firstNameAlias: null,
+        addressAlias: null,
+        dateOfBirthAlias: null,
+        driversLicenseAlias: null,
+        genderAlias: null,
+        genericAlias: null,
+        lastNameAlias: null,
+        passportAlias: null,
+        placeOfBirthAlias: null,
+        raceAlias: null,
+        randomAlias: null,
+        vehicleRegistrationAlias: null,
     };
 
     app.engine('hbs', handlebars({
@@ -42,20 +42,20 @@ const start = async () => {
     app.get('/reset', (req, res) => {
         exampleStore.vaultObj = null;
         exampleStore.communicationVaultObj = null;
-        exampleStore.emailToken = null;
-        exampleStore.genericToken = null;
-        exampleStore.ssnToken = null;
-        exampleStore.firstNameToken = null;
-        exampleStore.addressToken = null;
-        exampleStore.dateOfBirthToken = null;
-        exampleStore.driversLicenseToken = null;
-        exampleStore.genderToken = null;
-        exampleStore.lastNameToken = null;
-        exampleStore.passportToken = null;
-        exampleStore.placeOfBirthToken = null;
-        exampleStore.raceToken = null;
-        exampleStore.randomToken = null;
-        exampleStore.vehicleRegistrationToken = null;
+        exampleStore.emailAlias = null;
+        exampleStore.genericAlias = null;
+        exampleStore.ssnAlias = null;
+        exampleStore.firstNameAlias = null;
+        exampleStore.addressAlias = null;
+        exampleStore.dateOfBirthAlias = null;
+        exampleStore.driversLicenseAlias = null;
+        exampleStore.genderAlias = null;
+        exampleStore.lastNameAlias = null;
+        exampleStore.passportAlias = null;
+        exampleStore.placeOfBirthAlias = null;
+        exampleStore.raceAlias = null;
+        exampleStore.randomAlias = null;
+        exampleStore.vehicleRegistrationAlias = null;
         res.redirect('/');
     });
 
@@ -70,74 +70,74 @@ const start = async () => {
     });
 
     app.post('/communication/email', async (req, res) => {
-        exampleStore.emailToken = await exampleStore.communicationVaultObj.email.postEmail(req.body.email, ['example-email-tag']);
+        exampleStore.emailAlias = await exampleStore.communicationVaultObj.email.postEmail(req.body.email, ['example-email-tag']);
         res.redirect('/');
     });
 
     app.post('/static/generic', async (req, res) => {
-        exampleStore.genericToken = await exampleStore.vaultObj.generic.postGeneric(req.body.generic, req.body.template, ['example-generic-tag']);
+        exampleStore.genericAlias = await exampleStore.vaultObj.generic.postGeneric(req.body.generic, req.body.template, ['example-generic-tag']);
         res.redirect('/');
     });
 
     app.post('/static/ssn', async (req, res) => {
-        exampleStore.ssnToken = await exampleStore.vaultObj.ssn.postSSN(req.body.ssn, ['example-ssn-tag']);
+        exampleStore.ssnAlias = await exampleStore.vaultObj.ssn.postSSN(req.body.ssn, ['example-ssn-tag']);
         res.redirect('/');
     });
 
     app.post('/static/firstName', async (req, res) => {
-        exampleStore.firstNameToken = await exampleStore.vaultObj.firstName.postFirstName(req.body.firstName, ['example-firstName-tag']);
+        exampleStore.firstNameAlias = await exampleStore.vaultObj.firstName.postFirstName(req.body.firstName, ['example-firstName-tag']);
         res.redirect('/');
     });
 
     app.post('/static/address', async (req, res) => {
-        exampleStore.addressToken = await exampleStore.vaultObj.address.postAddress(req.body.address, ['example-address-tag']);
+        exampleStore.addressAlias = await exampleStore.vaultObj.address.postAddress(req.body.address, ['example-address-tag']);
         res.redirect('/');
     });
 
     app.post('/static/dateOfBirth', async (req, res) => {
-        exampleStore.dateOfBirthToken = await exampleStore.vaultObj.dateOfBirth.postDateOfBirth(req.body.dateOfBirth, ['example-dateofbirth-tag']);
+        exampleStore.dateOfBirthAlias = await exampleStore.vaultObj.dateOfBirth.postDateOfBirth(req.body.dateOfBirth, ['example-dateofbirth-tag']);
         res.redirect('/');
     });
 
     app.post('/static/driversLicense', async (req, res) => {
-        exampleStore.driversLicenseToken = await exampleStore.vaultObj.driversLicense
+        exampleStore.driversLicenseAlias = await exampleStore.vaultObj.driversLicense
             .postDriversLicense(req.body.driversLicense, ['example-driverslicense-tag']);
         res.redirect('/');
     });
 
     app.post('/static/gender', async (req, res) => {
-        exampleStore.genderToken = await exampleStore.vaultObj.gender.postGender(req.body.gender, ['example-gender-tag']);
+        exampleStore.genderAlias = await exampleStore.vaultObj.gender.postGender(req.body.gender, ['example-gender-tag']);
         res.redirect('/');
     });
 
     app.post('/static/lastName', async (req, res) => {
-        exampleStore.lastNameToken = await exampleStore.vaultObj.lastName.postLastName(req.body.lastName, ['example-lastName-tag']);
+        exampleStore.lastNameAlias = await exampleStore.vaultObj.lastName.postLastName(req.body.lastName, ['example-lastName-tag']);
         res.redirect('/');
     });
 
     app.post('/static/passport', async (req, res) => {
-        exampleStore.passportToken = await exampleStore.vaultObj.passport.postPassport(req.body.passport, ['example-passport-tag']);
+        exampleStore.passportAlias = await exampleStore.vaultObj.passport.postPassport(req.body.passport, ['example-passport-tag']);
         res.redirect('/');
     });
 
     app.post('/static/placeOfBirth', async (req, res) => {
-        exampleStore.placeOfBirthToken = await exampleStore.vaultObj.placeOfBirth
+        exampleStore.placeOfBirthAlias = await exampleStore.vaultObj.placeOfBirth
             .postPlaceOfBirth(req.body.placeOfBirth, ['example-placeofbirth-tag']);
         res.redirect('/');
     });
 
     app.post('/static/race', async (req, res) => {
-        exampleStore.raceToken = await exampleStore.vaultObj.race.postRace(req.body.race, ['example-race-tag']);
+        exampleStore.raceAlias = await exampleStore.vaultObj.race.postRace(req.body.race, ['example-race-tag']);
         res.redirect('/');
     });
 
     app.post('/static/random', async (req, res) => {
-        exampleStore.randomToken = await exampleStore.vaultObj.random.postRandom(req.body.random, ['example-random-tag']);
+        exampleStore.randomAlias = await exampleStore.vaultObj.random.postRandom(req.body.random, ['example-random-tag']);
         res.redirect('/');
     });
 
     app.post('/static/vehicleRegistration', async (req, res) => {
-        exampleStore.vehicleRegistrationToken = await exampleStore.vaultObj.vehicleRegistration
+        exampleStore.vehicleRegistrationAlias = await exampleStore.vaultObj.vehicleRegistration
             .postVehicleRegistration(req.body.vehicleRegistration, ['example-vehicleregistration-tag']);
         res.redirect('/');
     });
